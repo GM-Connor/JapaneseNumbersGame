da0fa860207b1f8665e7da2b98c4ea1bfe1bf3a7
Connor Gray <csgray1414@gmail.com>
2016-11-16 20:26:38 -0500

    change difficulty implemented

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,62 @@
-function changeDifficulty(difficulty) {
-	console.log(difficulty);
+function addDifficultyListeners(difficulties) {
+	for (var i = 0; i < difficulties.length; ++i)
+		(function(difficulty) {
+			$('.sidebar #' + difficulty)[0].addEventListener('click', function() {
+				changeDifficulty(difficulty);
+			});
+		})(difficulties[i]);
 }
-$('.sidebar .easy')[0].addEventListener('click', function() {
-	changeDifficulty("easy");
-});
-$('.sidebar .medium')[0].addEventListener('click', function() {
-	changeDifficulty("medium");
-});
-$('.sidebar .hard')[0].addEventListener('click', function() {
-	changeDifficulty("hard");
-});
-$('.sidebar .expert')[0].addEventListener('click', function() {
-	changeDifficulty("expert");
-});
+
+
+function changeDifficulty(difficulty) {
+	deActive();
+	$(event.target).addClass('active');
+	changeBackground(difficulty);
+	changeDiffText(difficulty);
+}
+/* removes .active class from all difficulties in sidebar */
+function deActive() {
+	currently_active = $('.sidebar .difficulty .active');
+	for (var i = 0; i < currently_active.length; ++i)
+		$(currently_active[i]).removeClass('active');
+}
+function changeBackground(difficulty) {
+	removeBackground();
+	switch(difficulty) {
+		case 'easy':
+			c = 'progress-bar-info';
+			break;
+		case 'medium':
+			c = 'progress-bar-warning';
+			break;
+		case 'hard':
+			c = 'progress-bar-danger';
+			break;
+		case 'expert':
+			c = 'progress-bar-success';
+			break;
+		default:
+			console.log('changeBackground(difficulty): unknown difficulty: ' + difficulty);
+			break;
+	}
+	$('#background').addClass(c);
+}
+function removeBackground() {
+	var options = ['info', 'warning', 'danger', 'success'];
+	for (var i = 0; i < options.length; ++i)
+		$('#background').removeClass('progress-bar-' + options[i]);
+}
+function changeDiffText(difficulty) {
+	$('.main .heading .difficulty span')[0].innerHTML = capitalize(difficulty);
+}
+function capitalize(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+
+
+var difficulties = ['easy', 'medium', 'hard', 'expert'];
+
+addDifficultyListeners(difficulties);
+$('.sidebar #' + difficulties[0])[0].click();	/* set default difficulty (first element in difficulties[]) */
